0ba0bcb4c5936463e64648caee76e7ec069bd9fd
Shivam Rathod <shivam.rathod@divergentsl.com>
2019-12-08 18:17:56 +0530

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FhgcjComponent } from './fhgcj/fhgcj.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      FhgcjComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/fhgcj/fhgcj.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fhgcj',
+  templateUrl: './fhgcj.component.html',
+  styleUrls: ['./fhgcj.component.css']
+})
+export class FhgcjComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
